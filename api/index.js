cb756d9f62804077ccb18814dfb31fbf4ac4611c
Bayron Carranza <bayron.carranza@gmail.com>
2024-05-11 15:22:36 -0600

    Develop (#3)
    Release 1

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World F1.F2!');
 })
 
 app.post('/api/transaction', async (req, res) => {
