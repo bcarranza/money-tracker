c3f5a1b6f14e68f88a28461bc62668b311133fc0
bcarranza <bayron.carranza@gmail.com>
2024-05-11 15:08:39 -0600

    se cambio hello wolrd y place holder

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World Feature 1!');
 })
 
 app.post('/api/transaction', async (req, res) => {
